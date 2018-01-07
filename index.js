6002a022524d5d34feac1723224febee8f4d6d23
Mike Bakker <equanoxproductions@gmail.com>
2018-01-07 15:27:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function logWhisper(string) {
   console.log(string.toLowerCase());
 }
 function sayHiToGrandma(string) {
-    return string.toLowerCase()
-    console.log('string')
+  if(string.toLowerCase)
+    return string 
 }
